d3e7f766e5f42c85fa36600ce92fd8177d10b9da
Kaustubh Shankar <kaustubhshan@gmail.com>
2020-01-07 12:12:42 +0530

    Update traffic_light.c

--- traffic_light.c
+++ traffic_light.c
@@ -11,8 +11,8 @@
 struct State
 {
 	unsigned char traffic_light_out;				//traffic light output, pedestrian crossing output
-	unsigned long time;											//state wait time (multiples of 10ms)
-	unsigned long next[8];									//next state depending on input and current state
+	unsigned long time;						//state wait time (multiples of 10ms)
+	unsigned long next[8];						//next state depending on input and current state
 };
 
 typedef struct State STATE;
@@ -29,11 +29,11 @@ STATE FSM[7] =
 		{0x49, 400, {goN, goN, goW, goW, goN, goN, goN, goN}}
 	};		// 7 FSM states. Based on Moore Model
 
-void systick_init(void);									//initialising systic timer for state wait time
-void portB_init(void);										//traffic light signal outputs
-void portD_init(void);	  								//button inputs - north road, west road, pedestrian crossing
+void systick_init(void);				//initialising systic timer for state wait time
+void portB_init(void);					//traffic light signal outputs
+void portA_init(void);	  				//button inputs - north road, west road, pedestrian crossing
 void wait_time(unsigned long value);			//delay time (multiples of 10ms)
-void toggle_pedestrian(void);							//toggle pedestrian green light for "hurryPed" state
+void toggle_pedestrian(void);				//toggle pedestrian green light for "hurryPed" state
 
 int main(void)
 {
@@ -41,19 +41,19 @@ int main(void)
 	unsigned char input = 0x00;
 	
 	portB_init();
-	portD_init();
+	portA_init();
 	
 	while(1)
 	{
-		GPIOB->DATA = FSM[current_state].traffic_light_out;					//traffic light output, pedestrian crossing light output
+		GPIOB->DATA = FSM[current_state].traffic_light_out;			//traffic light output, pedestrian crossing light output
 		
 		if(current_state == hurryPed)																//if in "hurryPed" state toggle green pedestrian crossing light
 			toggle_pedestrian();
 		else																												//wait time for the current state
 			wait_time(FSM[current_state].time);
 		
-		input = GPIOD->DATA;																				//taking input from buttons - PD2, PD1, PD0
-		current_state = FSM[current_state].next[input];							//next state = f(current state, input)
+		input = (GPIOA->DATA >> 2);																				//taking input from buttons - PD2, PD1, PD0
+		current_state = FSM[current_state].next[input];				//next state = f(current state, input)
 	}
 	
 	return 0;
@@ -61,9 +61,9 @@ int main(void)
 
 void systick_init(void)
 {
-	SysTick->CTRL = 0x04;					//enable = 0, clk_src = 1 (16 MHz Bus Clock)
+	SysTick->CTRL = 0x04;			//enable = 0, clk_src = 1 (16 MHz Bus Clock)
 	SysTick->LOAD = 160000 - 1;		//10ms count
-	SysTick->VAL = 0x00;					//to clear by writing to the current value register
+	SysTick->VAL = 0x00;			//to clear by writing to the current value register
 	SysTick->CTRL |= (1 << 0);		//enable and start timer
 }
 
@@ -74,17 +74,17 @@ void portB_init(void)
 	GPIOB->DEN |= 0xFF;
 	GPIOB->AFSEL &= ~(0xFF);
 	GPIOB->PCTL = 0x00000000;
-	GPIOB->DIR |= 0xFF;					//output pins - PB5, PB4, PB3, PB2, PB1, PB0 = traffic lights
+	GPIOB->DIR |= 0xFF;			//output pins - PB5, PB4, PB3, PB2, PB1, PB0 = traffic lights
 }
 
-void portD_init(void)
+void portA_init(void)
 {
-	SYSCTL->RCGCGPIO |= (1 << 3);
-	GPIOD->CR |= 0x07;
-	GPIOD->DEN |= 0x07;
-	GPIOD->AFSEL &= ~(0x07);
-	GPIOD->PCTL = 0x00000000;
-	GPIOD->DIR |= 0x00;					//input pins - PD2, PD1, PD0 = button inputs
+	SYSCTL->RCGCGPIO |= (1 << 0);
+	GPIOA->CR |= 0x1C;
+	GPIOA->DEN |= 0x1C;
+	GPIOA->AFSEL &= ~(0x1C);
+	GPIOA->PCTL = 0x00000000;
+	GPIOA->DIR |= 0x00;			//input pins - PA4, PA3, PA2 = button inputs
 }
 
 void wait_time(unsigned long value)
@@ -100,20 +100,21 @@ void wait_time(unsigned long value)
 
 void toggle_pedestrian(void)
 {
+	//Systick timer initialization 
 	systick_init();
 	
 	for(unsigned long count = 0; count < 3; count++)
 	{
-		GPIOE->DATA = 0x02;
+		GPIOB->DATA |= 0x80;
 		for(unsigned long loop = 0; loop < 100; loop++)	//total delay = 1s	
 		{
 			while((SysTick->CTRL & 0x10000) != 0x10000);	//inner loops gives 10ms delay
 		}
-	
-		GPIOE->DATA = 0x00;
+		GPIOB->DATA &= ~(0x80);
 		for(unsigned long loop = 0; loop < 100; loop++)	//total delay = 1s	
 		{
 			while((SysTick->CTRL & 0x10000) != 0x10000);	//inner loops gives 10ms delay
 		}
 	}
 }
+
